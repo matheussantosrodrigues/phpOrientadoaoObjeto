5e21bb9cb1a334365c55081bf7ba4b651a22af10
matheussantosrodrigues <matheusdasilva1005@gmail.com>
2024-04-14 03:11:54 -0300

    1 e 2 aula

--- primeiraAula/index.php
+++ primeiraAula/index.php
@@ -6,21 +6,29 @@ class Pessoa {
 
     // Criando um método
     public function Falar() {
-        echo "Falou";
+        echo $this->nome." de ".$this->idade." acabou de falar";
     }
 
 }
 
 // Instanciando uma classe
 $rodrigo = new Pessoa();
-var_dump
-class Carro {
-    public $nome;
-    public $idade;
 
-    // Criando um método
-    public function Falar() {
-        echo "Falou";
-    }
 
-}
+// var_dump($rodrigo);// esse negocio coisa o objeto.
+
+
+//Ao acessar o atributo nn precisa do 
+// cifrão
+$rodrigo->nome = "Rodrigo Santos de 
+Oliveira";
+$rodrigo->idade = 25;
+// echo "$rodrigo->nome <br>";
+// echo "Olá sou $rodrigo->nome tenho 
+// $rodrigo->idade anos.";
+
+$rodrigo->Falar();
+
+
+
+?>
