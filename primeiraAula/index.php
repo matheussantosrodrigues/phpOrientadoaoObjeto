ea54990e45d43c627d253a533b28d9f856ce0687
matheussantosrodrigues <matheusdasilva1005@gmail.com>
2024-04-14 02:48:47 -0300

    ai que nn sei oque

---
+++ primeiraAula/index.php
@@ -0,0 +1,26 @@
+<?php
+
+class Pessoa {
+    public $nome;
+    public $idade;
+
+    // Criando um método
+    public function Falar() {
+        echo "Falou";
+    }
+
+}
+
+// Instanciando uma classe
+$rodrigo = new Pessoa();
+var_dump
+class Carro {
+    public $nome;
+    public $idade;
+
+    // Criando um método
+    public function Falar() {
+        echo "Falou";
+    }
+
+}
